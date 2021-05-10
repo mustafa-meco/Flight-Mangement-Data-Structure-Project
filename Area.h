33ec079a2db3b91f0f80dcc00a85bfcc85a8fbba
MUSTAPHA TAYIL$ git config --global user.name MUSTAPHA <s-mustafa.tayel@zewailcity.edu.eg>
2021-05-10 15:47:26 +0200

    tayil2

--- Area.h
+++ Area.h
@@ -4,19 +4,21 @@ class Area
 
 private:
 	int AreaNum;
+	static int NumOfAreas;
 	static int** ArrOfAreas;
 	int NumOfLanes;
 public:
 
-	Area();
+	Area(int c);
 	int DisOfAreas(int);
 	int getNumLanes();
 
 };
 
-Area::Area()
+Area::Area(int c)
 {
-	AreaNum = 0;
+	AreaNum = c;
+	NumOfAreas++;
 }
 
 int Area::DisOfAreas(int A)
